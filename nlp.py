f92be9aeb4357e80f96bcd36992d9127600af9a6
Kaiwalya Patil <39062805+kaiwalya4850@users.noreply.github.com>
2020-01-02 15:10:01 +0530

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -8,6 +8,6 @@ from collections import Counter
 import en_core_web_sm
 import spacy
 nlp = spacy.load("en_core_web_sm")
-
+#git pull
 doc = nlp('Kaiwalya here, living in Anand, facing water logging problem since 2 hours near Sastri Medan')
 print([(X.text, X.label_) for X in doc.ents])
