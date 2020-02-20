37a0ee7b5780e7612c674f11b3b03255279cd046
Kaiwalya Patil <39062805+kaiwalya4850@users.noreply.github.com>
2020-02-20 23:21:03 +0530

    Data representation file

--- NLP/firestore_read.py
+++ NLP/firestore_read.py
@@ -1,5 +1,3 @@
-
-
 def read_entities(db):
 	docs = db.collection(u'NLP').stream()
 	return docs
---
+++ NLP/representation.py
@@ -0,0 +1,17 @@
+import firebase_admin
+import google.cloud
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("ftposs-9bbdd6e0ec3b.json")
+app = firebase_admin.initialize_app(cred)
+
+store = firestore.client()
+doc_ref = store.collection(u'REPORTS_CLASSIFIED')
+n=0
+try:
+    docs = doc_ref.stream()
+    for doc in docs:
+        print(u'Doc Data:{}'.format(doc.to_dict()))
+except:
+    print(u'Missing data')
+print(doc)
