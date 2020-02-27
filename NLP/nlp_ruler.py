e6177d26c173f983b56ee28065208cb5a45ced7b
Shikhar Maheshwari <37925362+Shikhar10000@users.noreply.github.com>
2020-02-27 14:25:43 +0530

    Update nlp_ruler.py

--- NLP/nlp_ruler.py
+++ NLP/nlp_ruler.py
@@ -20,10 +20,11 @@ def NLP_E(ip,db):
 
 
 	def add_ruler(entity_name,entity_arr):
-		ruler = EntityRuler(nlp, overwrite_ents=True)
+		ruler = EntityRuler(nlp)
 		for d in entity_arr:
-			ruler.add_patterns([{"label": str(entity_name), "pattern": d}])
+			ruler.add_patterns([{"label": str(entity_name), "pattern": str(d)}])
 		ruler.name = str(entity_name)
+		print('adding ent ',str(entity_name))
 		return ruler
 	nlp = spacy.load('en_core_web_sm')
 
@@ -36,7 +37,7 @@ def NLP_E(ip,db):
 	for doc in docs:
 		ruler=add_ruler(doc.id,doc.to_dict()['values'])
 
-		nlp.add_pipe(ruler)
+		nlp.add_pipe(ruler, before='ner')
 
 	#print(nlp.pipe_names) # Let's review what are the entities in our model
 
@@ -49,14 +50,14 @@ def NLP_E(ip,db):
 
 	print("Classifing ")
 	doc = nlp(sentence)
-	a = [(X.text, X.label_) for X in doc.ents]
+	# a = [(X.text, X.label_) for X in doc.ents]
 	#print(a) # List of the entities : reason
 	d = {}
 	tokenList = nltk.word_tokenize(sentence)
 	for ent in doc.ents:
-		if ent.label_ == "GPE" or ent.label_ == "NORP" or ent.label_ == "LOC":
-			d["Location"] = ent.text
-			#print('location = ',ent.text)
+		if ent.label_ == "GPE" or ent.label_ == "NORP" or ent.label_ == "LOC" or ent.label_ == "City":
+			d["Location "+str(ent.label_)] = ent.text
+			print('location = ',ent.text,ent.label_)
 		if ent.label_ == "TIME":
 			d["Time"] = ent.text
 	s = sentence.replace(',','')
@@ -65,7 +66,7 @@ def NLP_E(ip,db):
 	s = s.replace('?','')
 	s = s.lower()
 	doc = nlp(s)
-	a = [(X.text, X.label_) for X in doc.ents]
+	# a = [(X.text, X.label_) for X in doc.ents]
 	tokenList = nltk.word_tokenize(sentence)
 	for ent in doc.ents:
 		d[ent.label_] = ent.text
