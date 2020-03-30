1c36c2b2d4d7a344abdbc77b2c4a4c51403cf609
Kaiwalya Patil <39062805+kaiwalya4850@users.noreply.github.com>
2020-03-31 00:58:52 +0530

    getting individual counts

--- NLP/main_flask.py
+++ NLP/main_flask.py
@@ -1,5 +1,5 @@
-import firebase_admin
 import google.cloud
+import firebase_admin
 from firebase_admin import credentials, firestore
 
 cred = credentials.Certificate("ftposs-50575d1883e8.json")
@@ -7,6 +7,9 @@ app = firebase_admin.initialize_app(cred)
 
 store = firestore.client()
 doc_ref = store.collection(u'REPORTS_CLASSIFIED')
+#docs = doc_ref.where(u'Location', u'==', True).stream()
+
+# Getting data and storing in n#
 n=[]
 try:
     docs = doc_ref.stream()
@@ -15,17 +18,40 @@ try:
         n.append(a)
 except:
     print(u'Missing data')
-#print(n)
-k = 0
-keys = []
-value = []
+print(len(n))
+# Just in case query method fails and it did!
+# To get count of all entities and their values #
+def entity_cal(lst):
+    z = list(lst[0])
+    #print(z)
+    for i in range((len(lst)-1)):
+	    x = list(lst[i+1])
+	    for j in range(len(x)):
+		    y = x[j]
+		    z.append(y)
+		    j = j+1
+	    i = i+1
+    entity_list = list(set(z))
+    print(entity_list)
+    final = []
+    for i in range(len(entity_list)):
+	    k = entity_list[i]
+	    count = z.count(k)
+	    app = str(k) + ":" + str(count)
+	    final.append(app)
+	    i = i+1
+    return final
+
+# Getting values in a list "values" #
+values = []
 for i in range(len(n)):
-	z = n[k]
-	key = z.keys()
-	keys.append(key)
-	val = z.values()
-	value.append(val)
-	k = k+1
-print(keys[0][11])
+	k = list(n[i].values())
+	k.pop(-1) #since every last element is a timestamp
+	values.append(k)
+
+
+a = entity_cal(values)
+print(a)
+
 
 
