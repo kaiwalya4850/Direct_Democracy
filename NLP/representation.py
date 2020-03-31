5a9ffe5828e5278891555d572a909fdcfed9b400
Kaiwalya Patil <39062805+kaiwalya4850@users.noreply.github.com>
2020-03-31 23:36:32 +0530

    added small functions

--- NLP/representation.py
+++ NLP/representation.py
@@ -6,14 +6,38 @@ cred = credentials.Certificate("ftposs-50575d1883e8.json")
 app = firebase_admin.initialize_app(cred)
 
 store = firestore.client()
-doc_ref = store.collection(u'NLP')
-n={}
+doc_ref = store.collection(u'REPORTS_CLASSIFIED')
+n=[]
 try:
     docs = doc_ref.stream()
     for doc in docs:
-        print(u'Doc Data:{}'.format(doc.to_dict()))
         a = doc.to_dict()
-        n = a
+        n.append(a)
 except:
     print(u'Missing data')
-print(doc)
+print(type(n[0]))
+
+location_reports = []
+def checkKey(dict, key):      
+    if key in dict.keys(): 
+        #print("Present, value is", dict[key]) 
+        val = "yes"
+    else:
+        val = "no"
+    return val
+
+
+def report_gen(key_name,entity_reports_list):
+    for i in range(len(n)):
+        k = n[i]
+        c = checkKey(k,key_name)
+        if c is "yes":
+            entity_reports_list.append(k)
+        else:
+            pass
+
+
+
+report_gen("Location",location_reports)
+print(len(location_reports))
+    
