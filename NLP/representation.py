cb5a927274600ff6c1e2e50d1faa3da20d39c6ad
Kaiwalya Patil <39062805+kaiwalya4850@users.noreply.github.com>
2020-04-01 22:56:33 +0530

    testing file!!

--- NLP/representation.py
+++ NLP/representation.py
@@ -1,6 +1,11 @@
 import firebase_admin
 import google.cloud
 from firebase_admin import credentials, firestore
+from flask import Flask, render_template, request, redirect, url_for, session
+
+appf = Flask(__name__)
+appf.secret_key = 'any random string'
+
 
 cred = credentials.Certificate("ftposs-50575d1883e8.json")
 app = firebase_admin.initialize_app(cred)
@@ -37,7 +42,28 @@ def report_gen(key_name,entity_reports_list):
             pass
 
 
-
 report_gen("Location",location_reports)
 print(len(location_reports))
-    
+data = list(location_reports[0].keys())
+#print(data)
+
+# To get reports ready to be shown on Flask Website #
+# Pass above list with filtered reports #
+# Gives you a list named final_reports #
+final_reports = []
+def report_for_flask(upper_list,rep):
+    fin= ""
+    for i in range(len(upper_list)):
+        key_val = list(upper_list[i].keys())
+        val_val = list(upper_list[i].values())
+        for l in range(len(key_val)):
+            stra = str(key_val[l])
+            strb = str(val_val[l])
+            strc = stra+": "+strb+ ", "
+            fin = fin+strc
+        i = i+1
+        rep.append(fin)
+
+
+report_for_flask(location_reports,final_reports)
+print((final_reports))
