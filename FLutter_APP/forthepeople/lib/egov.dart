cf4cffb50588590faff3e89e7ef98cc63739a0c3
Shikhar Maheshwari <37925362+Shikhar10000@users.noreply.github.com>
2020-04-03 09:44:13 +0530

    egov.dart

---
+++ FLutter_APP/forthepeople/lib/egov.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_form_bloc/flutter_form_bloc.dart';
+import 'package:form_bloc/form_bloc.dart';
+import 'package:forthepeople/sign_in.dart';
+
+import 'package:forthepeople/widgets/form_button.dart';
+import 'package:forthepeople/widgets/widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'login_page.dart';
+import 'sign_in.dart';
+
+
+class Egov extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
