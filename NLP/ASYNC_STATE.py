7f5902daf590ce800f86d5dc896680615ecb21a3
Shikhar Maheshwari <37925362+Shikhar10000@users.noreply.github.com>
2020-06-01 22:58:20 +0530

    Update ASYNC_STATE.py

--- NLP/ASYNC_STATE.py
+++ NLP/ASYNC_STATE.py
@@ -3,6 +3,9 @@ from firestore_add import get_UCreports,add_feed_to_WEreports,del_UCreports,get_
 from nlp_ruler import NLP_E
 import time
 db=firestore_auth.auth()
+doc_ref_ = db.collection(u'Async').document('State')
+dic={u'val':'1'}
+doc_ref_.set(dic)
 doc_ref = db.collection(u'Async').document(u'State')
 def on_snapshot(doc_snapshot, changes, read_time):
 	for doc in doc_snapshot:
@@ -10,10 +13,18 @@ def on_snapshot(doc_snapshot, changes, read_time):
 		if dic['val']=='1':
 #################################get UnPRocessed IDS###############################################
 			doc=get_UCreports(db)
+			docs=[]
+			for ids in doc:
+				docs.append(ids.id)
+			print(docs)
+			
 #################################get UnPRocessed IDS###############################################
 #################################Process tweets####################################################
-			for ids in doc:
-				report=get_reports(ids.id,db)
+			i=1
+			for ids in docs:
+			i=i+1
+				print(ids)
+				report=get_reports(ids,db)
 				d=NLP_E(report,db)
 				
 				print("Report: ",report)
@@ -21,13 +32,21 @@ def on_snapshot(doc_snapshot, changes, read_time):
 				print(d)
 				if len(d)==0:
 					print("unclassified, add to human classifer")
-					add_feed_to_WEreports(ids.id,db)
+					add_feed_to_WEreports(ids,db)
 				if len(d)<=2:
-					add_feed_to_WEreports(ids.id,db)
+					add_feed_to_WEreports(ids,db)
 					print("weakly classified, add to human classifier")
 					
-				push_reports_classified(d,ids.id,db)
-				del_UCreports(db,ids.id)
+				push_reports_classified(d,ids,db)
+				del_UCreports(db,ids)
+				if i==len(docs):
+					doc_ref_ = db.collection(u'Async').document('State')
+					dic={u'val':'0'}
+					doc_ref_.set(dic)
+			if len(docs)==0:
+				doc_ref_ = db.collection(u'Async').document('State')
+				dic={u'val':'0'}
+				doc_ref_.set(dic)
 #################################Process tweets####################################################
 			
 		
